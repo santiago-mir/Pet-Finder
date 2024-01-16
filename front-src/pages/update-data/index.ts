d7ea4916f302df5e9ee48933c5ab44ed549eada8
Santiago <san_chan97@hotmail.com>
2024-01-16 16:12:10 -0300

    update html en update-data-page

--- front-src/pages/update-data/index.ts
+++ front-src/pages/update-data/index.ts
@@ -5,12 +5,32 @@ class UpdateData extends HTMLElement {
   connectedCallback() {
     this.render();
   }
-  addListeners() {}
+  addListeners() {
+    const formEl = this.querySelector(".form");
+    formEl?.addEventListener("submit", (event) => {
+      event.preventDefault();
+      let target = event.target as any; // cast target;
+      console.log(typeof target.name.value, typeof target.city.value);
+    });
+  }
 
   render() {
     this.innerHTML = `
     <custom-header></custom-header>
-    soy el update data
+    <div class="login-container">
+    <h1>Tus Datos Personales</h1>
+    <form class="form">
+    <label class="label">
+    Nombre
+    <input class="input" name="name" type="text" />
+    </label>
+    <label class="label">
+    Localidad
+    <input class="input" name="city" type="text" />
+    </label>
+    <button class="button">Guardar</button>
+    </form>
+    </div>
     `;
     this.addListeners();
   }
